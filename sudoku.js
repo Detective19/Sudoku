d7e9d3f1d20590195bfc68e90eae8bddef0ac44c
Anurag Tomar <anuragtomar919@gmail.com>
2025-05-07 12:34:00 +0530

    Update sudoku.js

--- sudoku.js
+++ sudoku.js
@@ -1,10 +1,8 @@
+let numSelected = null;
+let tileSelected = null;
+let errors = 0;
 
-var numSelected = null;
-var tileSelected = null;
-
-var errors = 0;
-
-var board = [
+const board = [
     "--74916-5",
     "2---6-3-9",
     "-----7-1-",
@@ -14,9 +12,9 @@ var board = [
     "9-4-7---2",
     "67-83----",
     "81--45---"
-]
+];
 
-var solution = [
+const solution = [
     "387491625",
     "241568379",
     "569327418",
@@ -26,17 +24,16 @@ var solution = [
     "934176852",
     "675832941",
     "812945763"
-]
+];
 
-window.onload = function() {
+window.onload = function () {
     setGame();
-}
+};
 
 function setGame() {
     for (let i = 1; i <= 9; i++) {
-        
-        let number = document.createElement("div");
-        number.id = i
+        const number = document.createElement("div");
+        number.id = i;
         number.innerText = i;
         number.addEventListener("click", selectNumber);
         number.classList.add("number");
@@ -45,18 +42,17 @@ function setGame() {
 
     for (let r = 0; r < 9; r++) {
         for (let c = 0; c < 9; c++) {
-            let tile = document.createElement("div");
-            tile.id = r.toString() + "-" + c.toString();
-            if (board[r][c] != "-") {
+            const tile = document.createElement("div");
+            tile.id = `${r}-${c}`;
+
+            if (board[r][c] !== "-") {
                 tile.innerText = board[r][c];
                 tile.classList.add("tile-start");
             }
-            if (r == 2 || r == 5) {
-                tile.classList.add("horizontal-line");
-            }
-            if (c == 2 || c == 5) {
-                tile.classList.add("vertical-line");
-            }
+
+            if (r === 2 || r === 5) tile.classList.add("horizontal-line");
+            if (c === 2 || c === 5) tile.classList.add("vertical-line");
+
             tile.addEventListener("click", selectTile);
             tile.classList.add("tile");
             document.getElementById("board").append(tile);
@@ -64,31 +60,41 @@ function setGame() {
     }
 }
 
-function selectNumber(){
-    if (numSelected != null) {
+function selectNumber() {
+    if (numSelected) {
         numSelected.classList.remove("number-selected");
     }
+
     numSelected = this;
     numSelected.classList.add("number-selected");
 }
 
 function selectTile() {
-    if (numSelected) {
-        if (this.innerText != "") {
-            return;
-        }
+    if (!numSelected) return;
 
+    if (tileSelected) {
+        tileSelected.classList.remove("tile-selected");
+    }
 
-        let coords = this.id.split("-"); 
-        let r = parseInt(coords[0]);
-        let c = parseInt(coords[1]);
+    tileSelected = this;
+    tileSelected.classList.add("tile-selected");
 
-        if (solution[r][c] == numSelected.id) {
-            this.innerText = numSelected.id;
-        }
-        else {
-            errors += 1;
-            document.getElementById("errors").innerText = errors;
-        }
+    if (tileSelected.innerText !== "") return;
+
+    const [r, c] = tileSelected.id.split("-").map(Number);
+
+    if (solution[r][c] === numSelected.id) {
+        tileSelected.innerText = numSelected.id;
+        document.getElementById("message").innerText = "";
+    } else {
+        errors++;
+        tileSelected.classList.add("error-tile");
+        setTimeout(() => {
+            tileSelected.classList.remove("error-tile");
+        }, 500);
+
+        document.getElementById("errors").innerText = errors;
+        document.getElementById("message").innerText = "Wrong move!";
     }
 }
+
